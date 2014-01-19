00815a0dde1b0487f80a28f457ca8d97c6735b95
wxxweb <wxxweb@gmail.com>
2014-01-20 02:33:21 +0800

    向 stdafx.h 中添加 strsafe.h，并禁止加载 cstdio、cstring、cwchar

--- source/common/stdafx.h
+++ source/common/stdafx.h
@@ -7,21 +7,26 @@
 
 #include "targetver.h"
 
-#define WIN32_LEAN_AND_MEAN             //  从 Windows 头文件中排除极少使用的信息
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的信息
+#define _CSTDIO_						// 禁止加载 C 语言头文件 cstdio
+#define _CSTRING_						// 禁止加载 C 语言头文件 cstring 
+#define _CWCHAR_						// 禁止加载 C 语言头文件 cwchar
 
 // Windows 头文件:
 #include <ShlObj.h>
 #include <Shlwapi.h>
 #include <tchar.h>
 #include <windows.h>
-
-// C 运行时头文件
-#include <assert.h>
-#include <stdio.h>
-#include <time.h>
+#include <strsafe.h>
 
 // C++库函数
+#include <algorithm>
+#include <map>
 #include <queue>
+
+// C 运行时头文件
+//#include <cassert>
+#include <ctime>
 
 // TODO: 在此处引用程序需要的其他头文件
 #include "debug.h"
