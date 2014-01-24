29242f117a21b31fa2e41fc7578edd414835b1af
wxxweb <wxxweb@gmail.com>
2014-01-25 00:59:44 +0800

    添加 crypto.dll 内含第三方加密功能开源代码

---
+++ third/source/crypto/crypto.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * 文件：crypto.h
+ * 描述：包含 crypto 加密模块各公用头文件
+ * 作者：	wu.xiongxing
+ * 邮箱：	wxxweb@gmail.com
+ * 日期：	2013-01-25
+ ******************************************************************************/
+
+#ifndef __W2X_CRYPTO_CRYPTO_H__
+#define __W2X_CRYPTO_CRYPTO_H__
+
+#include "exports.h"
+#include "md5.h"
+
+#endif /* __W2X_CRYPTO_CRYPTO_H__ */
---
+++ third/source/crypto/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的信息
+#define _CSTDIO_						// 禁止加载 C 语言头文件 cstdio
+#define _CSTRING_						// 禁止加载 C 语言头文件 cstring 
+#define _CWCHAR_						// 禁止加载 C 语言头文件 cwchar
+
+// Windows 头文件:
+#include <Windows.h>
+
+// C++库函数
+
+// C 运行时头文件
+
+
+// TODO: 在此处引用程序需要的其他头文件
