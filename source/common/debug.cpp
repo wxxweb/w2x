e1dab5bb96ab495841432776e3d9b8c92927e1f5
wu.xiongxing <wu.xiongxing@utooo.com>
2014-02-03 14:58:52 +0800

    Release下捕获程序的参数异常和纯虚函数调用异常

--- source/common/debug.cpp
+++ source/common/debug.cpp
@@ -19,7 +19,7 @@ W2X_DEFINE_NAME_SPACE_BEGIN(debug)
 static FExcptionHandler gs_handler_ptr = NULL;
 
 
-void SetReportMode(EReportMode mode)
+W2X_COMMON_API void SetReportMode(EReportMode mode)
 {
 	if (kModeDebug == mode)
 	{ 
@@ -41,7 +41,7 @@ void SetReportMode(EReportMode mode)
 }
 
 
-void EnableLeakCheck(bool delay)
+W2X_COMMON_API void EnableLeakCheck(bool delay)
 {
 	// Get the current state of the flag and store it in a temporary variable.
 	int tmpDbgFlag = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
@@ -75,18 +75,26 @@ static LONG WINAPI HandleTopLevelException(EXCEPTION_POINTERS *_excption_ptr)
 {
 	PEXCEPTION_RECORD record_ptr = _excption_ptr->ExceptionRecord;
 
-	/* 获取异常模块全路径 */ 
-	HMODULE module_handle = NULL;
-	::GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
-		reinterpret_cast<LPCTSTR>(record_ptr->ExceptionAddress), &module_handle);
+	/* 获取主程序根全路径 */
+	TCHAR app_root_patch[MAX_PATH] = TEXT("");
+	::GetModuleFileName(NULL, app_root_patch, sizeof(app_root_patch));
+	::PathRemoveFileSpec(app_root_patch);
 
-	TCHAR module_path[MAX_PATH] = TEXT("");
-	::GetModuleFileName(module_handle, module_path, sizeof(module_path));
+	/* 获取异常模块全路径 */ 
+	HMODULE exception_module_handle = NULL;
+	::GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
+		reinterpret_cast<LPCTSTR>(record_ptr->ExceptionAddress), 
+		&exception_module_handle);
+
+	TCHAR exception_module_path[MAX_PATH] = TEXT("");
+	::GetModuleFileName(exception_module_handle, exception_module_path, 
+		sizeof(exception_module_path));
 
 	/* 生成 MiniDump 文件 */
 	TCHAR dump_file_path[MAX_PATH] = TEXT("");
-	_tcscpy_s(dump_file_path, _countof(dump_file_path), module_path);
-	_tcscat_s(dump_file_path, _countof(dump_file_path), TEXT(".dmp"));
+	_stprintf_s(dump_file_path, TEXT("%s\\%s.0x%08x_0x%08x.dmp"),
+		app_root_patch, ::PathFindFileName(exception_module_path),
+		record_ptr->ExceptionCode, record_ptr->ExceptionAddress);
 
 	HANDLE dump_file_handle = ::CreateFile(dump_file_path, GENERIC_WRITE, 
 		0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -107,7 +115,7 @@ static LONG WINAPI HandleTopLevelException(EXCEPTION_POINTERS *_excption_ptr)
 	if (NULL != gs_handler_ptr)
 	{
 		ExcptionInfo excption_info;
-		excption_info.module_name = module_path;
+		excption_info.module_name = exception_module_path;
 		excption_info.excption_code = record_ptr->ExceptionCode;
 		excption_info.excption_address = record_ptr->ExceptionAddress;
 		excption_info.excption_flags = record_ptr->ExceptionFlags;
@@ -126,9 +134,10 @@ static LONG WINAPI HandleTopLevelException(EXCEPTION_POINTERS *_excption_ptr)
 	TCHAR excption_str[1024] = TEXT("");
 	_stprintf_s(excption_str,
 		TEXT("A excption occurs in module(%s), code(0x%08x), ")
-		TEXT("address(0x%08x), flags(0x%08x), %d parameters."),
-		module_path, record_ptr->ExceptionCode, record_ptr->ExceptionAddress,
-		record_ptr->ExceptionFlags, record_ptr->NumberParameters);
+		TEXT("address(0x%08x), flags(0x%08x), %d parameters, dump(%s)"),
+		exception_module_path, record_ptr->ExceptionCode, 
+		record_ptr->ExceptionAddress, record_ptr->ExceptionFlags, 
+		record_ptr->NumberParameters, dump_file_path);
 
 	w2x::log::Custom custom;
 	custom.category = w2x::log::kCategoryWarn;
@@ -136,17 +145,61 @@ static LONG WINAPI HandleTopLevelException(EXCEPTION_POINTERS *_excption_ptr)
 	custom.is_immediately = true;
 
 	w2x::log::Log(&custom, excption_str);
-
 	::MessageBox(NULL, excption_str, TEXT("Excption"), MB_ICONERROR);
 
 	return EXCEPTION_CONTINUE_SEARCH;
 }
 
+static void HandleInvalidParameter(
+	LPCTSTR expression,
+	LPCTSTR function_name,
+	LPCTSTR file_name,
+	UINT line_code,
+	UINT_PTR reserved_ptr)
+{
+	// function_name、file_name、line_code 在 Release 下无效
+
+	TCHAR excption_str[1024] = TEXT("");
+	_stprintf_s(excption_str,
+		TEXT("Invalid parameter detected in function(%s), ")
+		TEXT("file(%s), line(%d), expression: %s"), 
+		function_name, file_name, line_code, expression);
+
+	w2x::log::Custom custom;
+	custom.category = w2x::log::kCategoryWarn;
+	custom.priority = w2x::log::kPriorityUrgent;
+	custom.is_immediately = true;
+
+	w2x::log::Log(&custom, excption_str);
+	::MessageBox(NULL, excption_str, TEXT("Excption"), MB_ICONERROR);
+
+	// 必须抛出异常，否则无法定位错误位置
+	throw 1;
+}
+
+static void HandlePurecall(void)
+{
+	TCHAR excption_str[1024] = TEXT("");
+	_stprintf_s(excption_str, TEXT("In _purecall_handler."));
+
+	w2x::log::Custom custom;
+	custom.category = w2x::log::kCategoryWarn;
+	custom.priority = w2x::log::kPriorityUrgent;
+	custom.is_immediately = true;
+
+	w2x::log::Log(&custom, excption_str);
+	::MessageBox(NULL, excption_str, TEXT("Excption"), MB_ICONERROR);
+
+	// 必须抛出异常，否则无法定位错误位置
+	throw 1;
+}
 
 W2X_COMMON_API void EnableExcptionHandle(FExcptionHandler _handler_ptr)
 {
 	gs_handler_ptr = _handler_ptr;
 	::SetUnhandledExceptionFilter(HandleTopLevelException);
+	::_set_invalid_parameter_handler(HandleInvalidParameter);
+	::_set_purecall_handler(HandlePurecall);
 }
 
 
