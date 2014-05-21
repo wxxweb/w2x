67941ba124a23384384f7f2b154f3a07d004041a
wu.xiongxing <wu.xiongxing@utooo.com>
2014-05-21 11:09:45 +0800

    修改注释；添加文件系统模块

---
+++ src/w2x_common/file_sys.cpp
@@ -0,0 +1,87 @@
+/*******************************************************************************
+文件:	file_sys.cpp
+描述:	参见file_sys.h
+作者:	wu.xiongxing
+邮箱:	wxxweb@gmail.com
+日期:	2014-05-21
+修改:
+*******************************************************************************/
+
+#include "stdafx.h"
+#include "file_sys.h"
+
+W2X_NAME_SPACE_BEGIN
+W2X_DEFINE_NAME_SPACE_BEGIN(file)
+
+
+W2X_COMMON_API bool TraverseDirectory(LPCTSTR pszDirPath, 
+	FileEnumHandler pEnumProc, 
+	PVOID pParam,
+	UINT nHierarchy)
+{
+	if (NULL == pszDirPath || NULL == pEnumProc)
+	{
+		ASSERT(false);
+		return false;
+	}
+
+	if (FALSE == ::PathIsDirectory(pszDirPath))
+	{
+		ASSERT(false);
+		return false;
+	}
+
+	size_t nLen = _tcslen(pszDirPath);
+	TCHAR szDirPath[MAX_PATH] = TEXT("");
+	TCHAR szFindPath[MAX_PATH] = TEXT("");
+	_tcscpy_s(szDirPath, MAX_PATH, pszDirPath);
+	_tcscpy_s(szFindPath, MAX_PATH, pszDirPath);
+	_tcscpy_s(szFindPath + nLen, MAX_PATH - nLen, TEXT("\\*.*"));
+
+	WIN32_FIND_DATA wfd = {0};
+	HANDLE hFind = ::FindFirstFile(szFindPath, &wfd);
+	if (INVALID_HANDLE_VALUE == hFind)
+	{
+		ASSERT(false);
+		return false;
+	}
+	do {
+		if (FILE_ATTRIBUTE_DIRECTORY == wfd.dwFileAttributes &&
+			(0 == _tcscmp(wfd.cFileName, TEXT("."))
+			|| 0 == _tcscmp(wfd.cFileName, TEXT(".."))))
+		{
+			continue;
+		}
+
+		if (false == 
+			(*pEnumProc)(szDirPath, wfd.cFileName, wfd.dwFileAttributes, pParam))
+		{
+			::FindClose(hFind);
+			return false;
+		}
+
+		if (FILE_ATTRIBUTE_DIRECTORY == wfd.dwFileAttributes)
+		{
+			if (0 != nHierarchy && 1 >= nHierarchy)
+			{
+				continue;
+			}
+
+			_tcscpy_s(szFindPath + nLen + 1, MAX_PATH - nLen - 1, wfd.cFileName);
+			if (false == 
+				TraverseDirectory(szFindPath, pEnumProc, pParam, nHierarchy - 1))
+			{
+				::FindClose(hFind);
+				return false;
+			}
+		}
+	} 
+	while (FALSE != ::FindNextFile(hFind, &wfd));
+
+	::FindClose(hFind);
+	return true;
+}
+
+
+W2X_DEFINE_NAME_SPACE_END(file)
+W2X_NAME_SPACE_END
---
+++ src/w2x_common/file_sys.h
@@ -0,0 +1,38 @@
+/******************************************************************************
+文件:	file_sys.h
+描述:	定义与文件系统相关的功能。
+作者:	wu.xiongxing
+邮箱:	wxxweb@gmail.com
+日期:	2014-05-21
+*******************************************************************************/
+
+#ifndef __W2X_COMMON_FILE_SYS_H__
+#define __W2X_COMMON_FILE_SYS_H__
+
+#include "exports.h"
+#include "macros.h"
+#include <WTypes.h>
+
+W2X_NAME_SPACE_BEGIN
+W2X_DEFINE_NAME_SPACE_BEGIN(file)
+
+/** 文件枚举处理器回调函数类型指针, 返回true继续遍历, 返回false停止遍历。 */
+typedef bool (CALLBACK *FileEnumHandler)(
+	LPCTSTR pszDirPath,		///< 文件所在目录全路径
+	LPCTSTR pszFileName,	///< 不包含路径的文件名
+	DWORD dwFileAttributes,	///< 对应 WIN32_FIND_DATA 结构成员 dwFileAttributes
+	PVOID pParam			///< TraverseDirectory 的 pParam 参数
+);
+
+/** 遍历目录, 并对每个文件（包括目录）应用文件枚举处理函数。*/
+W2X_COMMON_API bool TraverseDirectory(
+	LPCTSTR pszDirPath,			///< 被遍历的目录全路径
+	FileEnumHandler pEnumProc,	///< 文件枚举处理回调函数
+	PVOID pParam = NULL,		///< 传给处理函数的参数
+	UINT dwHierarchy = 0		///< 最大遍历层级, 0为无限制
+	);
+
+W2X_DEFINE_NAME_SPACE_END(file)
+W2X_NAME_SPACE_END
+
+#endif ///< !__W2X_COMMON_FILE_SYS_H__
