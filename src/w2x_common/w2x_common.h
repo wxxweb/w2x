e6dff1ba5b37801eae2ae8b4465d8fe8ee26c227
wu.xiongxing <wu.xiongxing@utooo.com>
2014-07-03 00:16:07 +0800

    防止w2x_common模块中包含w2x_common.h

--- src/w2x_common/w2x_common.h
+++ src/w2x_common/w2x_common.h
@@ -1,8 +1,9 @@
 /******************************************************************************
-文件:	common.h
+文件:	w2x_common.h
 描述:	本头文件包含了 w2x_common 模块中所有导出头文件，仅以方便为目的，供外部使用。但是，
 		仍旧建议根据具体需求包含所对应的导出头文件，而非直接包含本头文件。直接包含可能会导致
-		编译的目标文件（.obj文件）增大，从而在某种程度上影响到编译和链接速度。
+		编译的目标文件（.obj文件）增大，从而在某种程度上影响到编译和链接速度。如果确实需要
+		包含本头文件，建议在外部项目模块的预定义头文件中包含，这样将不会影响编译和链接速度。
 		另外，严禁在整个 w2x 项目内部包含该头文件。
 作者:	wu.xiongxing
 时间:	2013-05-24
@@ -12,6 +13,7 @@
 #ifndef __W2X_COMMON_COMMON_H__
 #define __W2X_COMMON_COMMON_H__
 
+#ifndef W2X_COMMON_EXPORTS ///< 禁止在 w2x_common 模块内部包含本头文件
 
 #include "./base.h"
 #include "./command.h"
@@ -32,6 +34,8 @@
 #include "./thread.h"
 #include "./variable.h"
 #include "./version.h"
+
+#endif ///< !W2X_COMMON_EXPORTS
 
 
 #endif ///< !__W2X_COMMON_COMMON_H__
