d6806c3f4a441eb3cd8370bc1c8fc4424b8c9cb4
wu.xiongxing <wu.xiongxing@utooo.com>
2014-07-03 00:12:32 +0800

    将所有导出头文件放在w2x_common.h中

---
+++ src/w2x_common/w2x_common.h
@@ -0,0 +1,37 @@
+/******************************************************************************
+文件:	common.h
+描述:	本头文件包含了 w2x_common 模块中所有导出头文件，仅以方便为目的，供外部使用。但是，
+		仍旧建议根据具体需求包含所对应的导出头文件，而非直接包含本头文件。直接包含可能会导致
+		编译的目标文件（.obj文件）增大，从而在某种程度上影响到编译和链接速度。
+		另外，严禁在整个 w2x 项目内部包含该头文件。
+作者:	wu.xiongxing
+时间:	2013-05-24
+修改:	2014-07-02
+*******************************************************************************/
+
+#ifndef __W2X_COMMON_COMMON_H__
+#define __W2X_COMMON_COMMON_H__
+
+
+#include "./base.h"
+#include "./command.h"
+#include "./common.h"
+#include "./debug.h"
+#include "./encode.h"
+#include "./error.h"
+#include "./event.h"
+#include "./event_dispatcher.h"
+#include "./file_sys.h"
+#include "./macros.h"
+#include "./msg_loop.h"
+#include "./mutex.h"
+#include "./process.h"
+#include "./ref_ptr.h"
+#include "./sys_tray.h"
+#include "./system.h"
+#include "./thread.h"
+#include "./variable.h"
+#include "./version.h"
+
+
+#endif ///< !__W2X_COMMON_COMMON_H__
